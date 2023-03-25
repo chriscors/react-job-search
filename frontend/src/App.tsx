a1c0b35ddf19201774b5ac992a912cdd82f87f17
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-25 10:25:04 -0400

    Formats header correctly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header'
 import Sidebar from './components/sidebar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <Sidebar />
     </div>
   )
--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+// import * as React from 'react';
+import { FC, useState } from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -9,56 +10,46 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faNode, faPython, faReact} from '@fortawesome/free-brands-svg-icons'
 import { faGem } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeSvgIcon } from 'react-fontawesome-svg-icon';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 
-export default function MenuBar() {
-  function handleClick(
-    event: React.MouseEvent,
-    index: number,
-  ) {
-  
-}
-
+export default function Sidebar() {
+  const [activeCategory, setActiveCategory] = useState(null)
   return (
-    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <Box sx={{ height: '100vh', maxWidth: 200, overflowWrap: 'break-word', bgcolor: 'background.paper', boxShadow: 1}}>
       <nav aria-label='main sidebar'>
         <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <FontAwesomeSvgIcon icon = {faReact} />
-              </ListItemIcon>
-                <ListItemText primary="React" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <FontAwesomeIcon icon={faPython} />
-              </ListItemIcon>
-                <ListItemText primary="Python/Django" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <FontAwesomeIcon icon={faNode} />
-              </ListItemIcon>
-                <ListItemText primary="Node.js" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <FontAwesomeIcon icon={faGem} />
-              </ListItemIcon>
-                <ListItemText primary="Ruby/Rails" />
-            </ListItemButton>
-          </ListItem>
+          <MenuItem text = "React" icon = {faReact} />
+          <MenuItem text = "Python/Django" icon = {faReact} />
+          <MenuItem text = "Node.js" icon = {faNode} />
+          <MenuItem text = "Ruby/Rails" icon = {faGem} />
         </List>
       </nav>
 
     </Box>
   )
 
+}
+
+interface MenuProps {
+  text: string;
+  icon: IconProp
+}
+ 
+const MenuItem: FC<MenuProps> = ({ text, icon }) => {
+  function handleClick(
+    event: React.MouseEvent,
+  ) {
+  }
+
+  return (<>
+          <ListItem disablePadding>
+            <ListItemButton onClick={(e) => handleClick(e)}>
+              <ListItemIcon>
+                <FontAwesomeIcon icon = {icon} />
+              </ListItemIcon>
+                <ListItemText primary={text}/>
+            </ListItemButton>
+          </ListItem>
+  </>);
 }
