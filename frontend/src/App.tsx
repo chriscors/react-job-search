47ad1fd400ff30ce3717178895c0d9def11c3e29
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-25 11:02:58 -0400

    Fixes spelling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
 import Sidebar from './components/sidebar'
+import Accordion from "./components/accordion";
 
 function App() {
   const [activeCategory, setActiveCategory] = useState<null | string>(null)
