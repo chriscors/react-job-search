45279d240d4204247ca12442b685fe2f9ad1f2cf
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-25 01:07:12 -0400

    Gets Navbar up

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -5,8 +5,10 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faNode, faPython, faReact} from '@fortawesome/free-brands-svg-icons'
+import { faGem } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeSvgIcon } from 'react-fontawesome-svg-icon';
 
 
 export default function MenuBar() {
@@ -15,7 +17,36 @@ export default function MenuBar() {
       <nav aria-label='main sidebar'>
         <List>
           <ListItem disablePadding>
-
+            <ListItemButton>
+              <ListItemIcon>
+                <FontAwesomeSvgIcon icon = {faReact} />
+              </ListItemIcon>
+                <ListItemText primary="React" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FontAwesomeIcon icon={faPython} />
+              </ListItemIcon>
+                <ListItemText primary="Python/Django" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FontAwesomeIcon icon={faNode} />
+              </ListItemIcon>
+                <ListItemText primary="Node.js" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <FontAwesomeIcon icon={faGem} />
+              </ListItemIcon>
+                <ListItemText primary="Ruby/Rails" />
+            </ListItemButton>
           </ListItem>
         </List>
       </nav>
