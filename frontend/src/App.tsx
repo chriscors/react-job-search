9debb4172954150016078127ffef4a9e36a7ecde
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-25 01:07:32 -0400

    Empties basics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './components/sidebar'
 
 function App() {
   
 
   return (
     <div className="App">
-
+      <Sidebar />
     </div>
   )
 }
