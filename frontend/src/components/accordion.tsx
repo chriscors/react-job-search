0b9d98a06fd59ef0f7d5e7cb269f6b121aaafad2
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-25 11:25:58 -0400

    Adds useEffect hook

--- frontend/src/components/accordion.tsx
+++ frontend/src/components/accordion.tsx
@@ -1,14 +1,16 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faNode, faPython, faReact} from '@fortawesome/free-brands-svg-icons'
 import { faGem } from '@fortawesome/free-regular-svg-icons';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 
 
@@ -16,20 +18,56 @@ interface AccordionProps {
   activeCategory: string | null
   setActiveCategory: React.Dispatch<React.SetStateAction<null | string>>
 }
-const Accordion: FC<AccordionProps> = ({activeCategory, setActiveCategory}) => {
+const JobAccordion: FC<AccordionProps> = ({ activeCategory, setActiveCategory }) => {
+  
+  useEffect(() => {
+    let URL = null;
+
+    axios.get(URL, {
+      headers: {
+        Authorization: `Token ${import.meta.env.VITE_FINDWORK_API_KEY}`
+      }
+    })
+  }, [activeCategory])
+  
   
   return (
-    <Box sx={{ height: '100vh', maxWidth: 200, overflowWrap: 'break-word', bgcolor: 'background.paper', boxShadow: 1}}>
-      <nav aria-label='main sidebar'>
-        <List>
-          <MenuItem text = "React" icon = {faReact} activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
-          <MenuItem text="Python/Django" icon={faPython} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
-          <MenuItem text="Node.js" icon={faNode} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
-          <MenuItem text="Ruby/Rails" icon={faGem} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
-        </List>
-      </nav>
-
-    </Box>
+    <>
+      {/* Map the results of the axios */}
+    </>
   )
-
 }
+
+
+interface ListingProps {
+  activeCategory: string | null
+  setActiveCategory: React.Dispatch<React.SetStateAction<null | string>>
+}
+const JobListing = () => {
+  
+  return (
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<FontAwesomeIcon icon={faCaretDown} />}>
+          <Typography></Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography></Typography>
+        </AccordionDetails>
+      </Accordion>
+  )
+}
+
+// return (
+//   <Box sx={{ height: '100vh', maxWidth: 200, overflowWrap: 'break-word', bgcolor: 'background.paper', boxShadow: 1}}>
+//     <nav aria-label='main sidebar'>
+//       <List>
+//         <MenuItem text = "React" icon = {faReact} activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
+//         <MenuItem text="Python/Django" icon={faPython} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+//         <MenuItem text="Node.js" icon={faNode} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+//         <MenuItem text="Ruby/Rails" icon={faGem} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+//       </List>
+//     </nav>
+
+//   </Box>
+// )
